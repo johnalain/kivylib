e1b59b762077280b35e2f8e569a571231edf0603
john alain <johnalain572@gmail.com>
2024-02-27 18:48:41 +0200

    kivy project

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+class BoxLayoutExample(BoxLayout):
+    pass
+#https://youtu.be/l8Imtec4ReQ?t=1553
+class MainWidget(Widget):
+    pass
+class TheLabApp(App):
+    pass
+
+#https://youtu.be/l8Imtec4ReQ?t=1226
+TheLabApp().run()
