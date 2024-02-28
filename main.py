5839deccac57f3a592714c81e1063afe87b2de67
john alain <johnalain572@gmail.com>
2024-02-28 10:09:13 +0200

    size button

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
 class BoxLayoutExample(BoxLayout):
-    def __init__(self, **kwargs):
+    pass
+#u can add button here or in kv file i can use Label
+#instead of using Button
+    """def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.orientation = "vertical"
         b1 = Button(text="A")
         b2 = Button(text="B")
+        b3 = Button(text="c")
         self.add_widget(b1)
         self.add_widget(b2)
+        self.add_widget(b3)"""
         
 #https://youtu.be/l8Imtec4ReQ?t=1625
 class MainWidget(Widget):
