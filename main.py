a0986362a6c41eb8eac2d291cdde2af2a6b490ba
john alain <johnalain572@gmail.com>
2024-02-28 09:37:21 +0200

    add button

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from kivy.uix.button import Button
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
 class BoxLayoutExample(BoxLayout):
-    pass
-#https://youtu.be/l8Imtec4ReQ?t=1553
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        self.add_widget(b1)
+        self.add_widget(b2)
+        
+#https://youtu.be/l8Imtec4ReQ?t=1625
 class MainWidget(Widget):
     pass
 class TheLabApp(App):
