8c164a0e4d54c4e336f7063ba43a20acc14016bd
john alain <johnalain572@gmail.com>
2024-02-29 19:00:06 +0200

    positionning and sizing

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from kivy.uix.widget import Widget
 
 class BoxLayoutExample(BoxLayout):
     pass
+    # def __init__(self,**kwargs):
+    #     super(). __init__(**kwargs)
+    #     self.orientation ="vertical"
+    #     b1 = Button(text = "josephine")
+    #     b2 = Button(text = "rita")
+    #     b3 = Button(text = "michel")
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
 #u can add button here or in kv file i can use Label
 #instead of using Button
     """def __init__(self, **kwargs):
@@ -18,6 +27,7 @@ class BoxLayoutExample(BoxLayout):
         self.add_widget(b3)"""
         
 #https://youtu.be/l8Imtec4ReQ?t=1625
+#https://youtu.be/l8Imtec4ReQ?t=2022
 class MainWidget(Widget):
     pass
 class TheLabApp(App):
